a6e22e9a7c8133dc7f8aee5ecaeff2808c3aa448
Albert <reg16vp@mail.ru>
2022-11-27 21:57:44 +0300

    переделал alhpa как удобно, добавил telekasten и добавил на него мапинг

--- init.lua
+++ init.lua
@@ -72,36 +72,7 @@ local config = {
 
   -- Set dashboard header
   header = {
--- [[░░░░░░░░░░▀▀▀██████▄▄▄░░░░░░░░░░]],
--- [[░░░░░░░░░░░░░░░░░▀▀▀████▄░░░░░░░]],
--- [[░░░░░░░░░░▄███████▀░░░▀███▄░░░░░]],
--- [[░░░░░░░░▄███████▀░░░░░░░▀███▄░░░]],
--- [[░░░░░░▄████████░░░░░░░░░░░███▄░░]],
--- [[░░░░░██████████▄░░░░░░░░░░░███▌░]],
--- [[░░░░░▀█████▀░▀███▄░░░░░░░░░▐███░]],
--- [[░░░░░░░▀█▀░░░░░▀███▄░░░░░░░▐███░]],
--- [[░░░░░░░░░░░░░░░░░▀███▄░░░░░███▌░]],
--- [[░░░░▄██▄░░░░░░░░░░░▀███▄░░▐███░░]],
--- [[░░▄██████▄░░░░░░░░░░░▀███▄███░░░]],
--- [[░█████▀▀████▄▄░░░░░░░░▄█████░░░░]],
--- [[░████▀░░░▀▀█████▄▄▄▄█████████▄░░]],
--- [[░░▀▀░░░░░░░░░▀▀██████▀▀░░░▀▀██░░]],
-[[░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░]],
-[[░░░░░░░░░░░░░░░░░░░░░░░░░░░┌───┬───┬───┐░░░░░░░░░░░░░░░░░░░░░]],
-[[░░░░░░░░░┌─────────────┬░░░│┌─┐│┌──┴┐┌┐│░░░░░░░░░░░░░░░░░░░░░]],
-[[░░░░░░░┌─┘───────────┌─┤░░░│└─┘│░──┐││││░░░░░░░░░░░░░░░░░░░░░]],
-[[░░░░░┌─┘────────────┌┘╬│░░░│┌┐┌┤┌──┘││││░░░░░░░░░░░░░░░░░░░░░]],
-[[░░░░┌┘────────────┌─┘╬╬│░░░│││└┤│░░┌┘└┘│░░░░░░░░░░░░░░░░░░░░░]],
-[[░░┌─┘───────────┌─┘╬╬┌─┤░░░└┘└─┴┘░░└───┘░░░░░░░░░░░░░░░░░░░░░]],
-[[░░├─────────────┤╬╬┌─┘═│░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░]],
-[[░░│╬╬╬╬╬╬╬╬╬╬╬╬╬│╬─┘═══│░░░░┌┐┌─┐░┌┐░░░░░░░░░░░░░┌┐░░░░░░░░░░]],
-[[░░│╬╬╬╬╬╬╬╬╬┌───┼┘═══┌─┤░░░┌┘└┤│└┐││░░░░░░░░░░░░┌┘└┐░░░░░░░░░]],
-[[░░├─────────┘═══│══┌─┘║│░░░└┐┌┤┌┐└┘├──┬┐┌┬┬──┬──┼┐┌┼──┬─┐░░░░]],
-[[░░│═════════════│┌─┘║┌─┘░░░░││││└┐││┌┐│└┘├┤┌┐│┌┐││││┌┐│┌┘░░░░]],
-[[░░│═════┌───────┼┘║║┌┘░░░░░░│└┤│░│││┌┐├┐┌┤│└┘│┌┐││└┤└┘││░░░░░]],
-[[░░├─────┘║║║║║║║│║┌─┘░░░░░░░└─┴┘░└─┴┘└┘└┘└┴─┐├┘└┘└─┴──┴┘░░░░░]],
-[[░░└─────────────┴─┘░░░░░░░░░░░░░░░░░░░░░░░┌─┘│░░░░░░░░░░░░░░░]],
-[[░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░└──┘░░░░░░░░░░░░░░░]],
+
   },
 
   -- Default theme configuration
@@ -242,6 +213,31 @@ local config = {
       ["<leader>fp"] = { ":lua require('telescope').extensions.projects.projects()<CR>", desc = "project search" },
       ["<leader>ft"] = { ":TodoTelescope<CR>", desc = "todo_search" },
       ["<leader>ss"] = { ":lua require('spectre').open()<CR>", desc = "spectre" },
+      --telekastennnoremap <leader>zf :lua require('telekasten').find_notes()<CR>
+      ["<leader>zd"] = { ":lua require('telekasten').find_daily_notes()<CR>", desc="find_daily_notes"},
+      ["<leader>zg"] = { ":lua require('telekasten').search_notes()<CR>", desc="search_notes"},
+      ["<leader>zz"] = { ":lua require('telekasten').follow_link()<CR>", desc="follow_link"},
+      -- ["<leader>zT"] = { ":lua require('telekasten').goto_today()<CR>", desc="cast"},
+      -- ["<leader>zW"] = { ":lua require('telekasten').goto_thisweek()<CR>", desc="cast"},
+      -- ["<leader>zw"] = { ":lua require('telekasten').find_weekly_notes()<CR>", desc="cast"},
+      ["<leader>zn"] = { ":lua require('telekasten').new_note()<CR>", desc="new_note"},
+      ["<leader>zN"] = { ":lua require('telekasten').new_templated_note()<CR>", desc="new_templated_note"},
+      ["<leader>zy"] = { ":lua require('telekasten').yank_notelink()<CR>", desc="yank_notelink"},
+      ["<leader>zc"] = { ":lua require('telekasten').show_calendar()<CR>", desc="show_calendar"},
+      ["<leader>zC"] = { ":CalendarT<CR>", desc="CalendarT"},
+      -- TIP: разобраться что делают эти функции
+      ["<leader>zi"] = { ":lua require('telekasten').paste_img_and_link()<CR>", desc="paste_img_and_link"},
+      ["<leader>zt"] = { ":lua require('telekasten').toggle_todo()<CR>", desc="toggle_todo"},
+      ["<leader>zb"] = { ":lua require('telekasten').show_backlinks()<CR>", desc="show_backlinks"},
+      ["<leader>zF"] = { ":lua require('telekasten').find_friends()<CR>", desc="cast"},
+      ["<leader>zI"] = { ":lua require('telekasten').insert_img_link({ i=true })<CR>", desc="insert_img_link"},
+      ["<leader>zp"] = { ":lua require('telekasten').preview_img()<CR>", desc="preview_img"},
+      ["<leader>zm"] = { ":lua require('telekasten').browse_media()<CR>", desc="browse_media"},
+      ["<leader>za"] = { ":lua require('telekasten').show_tags()<CR>", desc="show_tags"},
+      -- ["<leader>#"] = { ":lua require('telekasten').show_tags()<CR>", desc="cast"},
+      ["<leader>zr"] = { ":lua require('telekasten').rename_note()<CR>", desc="rename_note"},
+
+
       ["<C-C>"] = { '"+yy', desc = "copy to global buffer" },
       ["<p>"] = { '"+gP', desc = "paste from global buffer" },
       -- quick save
@@ -382,7 +378,59 @@ local config = {
     -- }
   end,
 }
-
+-- NOTE: ALPHA dashboard setting
+local status_ok, alpha = pcall(require, "alpha")
+if not status_ok then
+  return
+end
+--
+local dashboard = require "alpha.themes.dashboard"
+--
+local function button(sc, txt, keybind, keybind_opts)
+  local b = dashboard.button(sc, txt, keybind, keybind_opts)
+  b.opts.hl_shortcut = "Macro"
+  return b
+end
+--
+--
+dashboard.section.header.val = {
+[[░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░]],
+[[░░░░░░░░░░░░░░░░░░░░░░░░░░░┌───┬───┬───┐░░░░░░░░░░░░░░░░░░░░░]],
+[[░░░░░░░░░┌─────────────┬░░░│┌─┐│┌──┴┐┌┐│░░░░░░░░░░░░░░░░░░░░░]],
+[[░░░░░░░┌─┘───────────┌─┤░░░│└─┘│░──┐││││░░░░░░░░░░░░░░░░░░░░░]],
+[[░░░░░┌─┘────────────┌┘╬│░░░│┌┐┌┤┌──┘││││░░░░░░░░░░░░░░░░░░░░░]],
+[[░░░░┌┘────────────┌─┘╬╬│░░░│││└┤│░░┌┘└┘│░░░░░░░░░░░░░░░░░░░░░]],
+[[░░┌─┘───────────┌─┘╬╬┌─┤░░░└┘└─┴┘░░└───┘░░░░░░░░░░░░░░░░░░░░░]],
+[[░░├─────────────┤╬╬┌─┘═│░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░]],
+[[░░│╬╬╬╬╬╬╬╬╬╬╬╬╬│╬─┘═══│░░░░┌┐┌─┐░┌┐░░░░░░░░░░░░░┌┐░░░░░░░░░░]],
+[[░░│╬╬╬╬╬╬╬╬╬┌───┼┘═══┌─┤░░░┌┘└┤│└┐││░░░░░░░░░░░░┌┘└┐░░░░░░░░░]],
+[[░░├─────────┘═══│══┌─┘║│░░░└┐┌┤┌┐└┘├──┬┐┌┬┬──┬──┼┐┌┼──┬─┐░░░░]],
+[[░░│═════════════│┌─┘║┌─┘░░░░││││└┐││┌┐│└┘├┤┌┐│┌┐││││┌┐│┌┘░░░░]],
+[[░░│═════┌───────┼┘║║┌┘░░░░░░│└┤│░│││┌┐├┐┌┤│└┘│┌┐││└┤└┘││░░░░░]],
+[[░░├─────┘║║║║║║║│║┌─┘░░░░░░░└─┴┘░└─┴┘└┘└┘└┴─┐├┘└┘└─┴──┴┘░░░░░]],
+[[░░└─────────────┴─┘░░░░░░░░░░░░░░░░░░░░░░░┌─┘│░░░░░░░░░░░░░░░]],
+[[░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░└──┘░░░░░░░░░░░░░░░]],
+}
+dashboard.section.buttons.val = {
+  button("f", "  Find file", ":Telescope find_files <CR>"),
+  button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+  button("p", " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
+  button("r", "  Recent files", ":Telescope oldfiles <CR>"),
+  button("t", "  Find text", ":Telescope live_grep <CR>"),
+  -- dashboard.button("s", icons.ui.SignIn .. " Find Session", ":silent Autosession search <CR>"),
+  button("s", "  Find Session", ":SessionManager load_session<CR>"),
+  -- TODO: сделать линк на bookmarks
+  --astronvim.alpha_button("LDR f m", "  Bookmarks  "),
+  button("c", " Config", ":e ~/.config/nvim/init.lua <CR>"),
+  button("u", "  Update", ":PackerSync<CR>"),
+  button("q", " Quit", ":qa<CR>"),
+}
+-- dashboard.section.header.opts.hl = "Include"
+-- dashboard.section.buttons.opts.hl = "Macro"
+--
+dashboard.opts.opts.noautocmd = true
+alpha.setup(dashboard.opts)
+--
 --NOTE: HOP plugin
 local status_ok, hop = pcall(require, "hop")
 if not status_ok then
@@ -709,6 +757,115 @@ spectre.setup({
 	replace_vim_cmd = "cdo",
 	is_open_target_win = true, --open file on opener window
 	is_insert_mode = false, -- start open panel on is_insert_mode
+})
+-- NOTE: Telekasten plugin
+require('telekasten').setup({
+    home         = home,
+
+    -- if true, telekasten will be enabled when opening a note within the configured home
+    take_over_my_home = true,
+
+    -- auto-set telekasten filetype: if false, the telekasten filetype will not be used
+    --                               and thus the telekasten syntax will not be loaded either
+    auto_set_filetype = true,
+
+    -- dir names for special notes (absolute path or subdir name)
+    dailies      = home .. '/' .. 'daily',
+    weeklies     = home .. '/' .. 'weekly',
+    templates    = home .. '/' .. 'templates',
+
+    -- image (sub)dir for pasting
+    -- dir name (absolute path or subdir name)
+    -- or nil if pasted images shouldn't go into a special subdir
+    image_subdir = "FILES",
+
+    -- markdown file extension
+    extension    = ".md",
+
+    -- following a link to a non-existing note will create it
+    follow_creates_nonexisting = true,
+    dailies_create_nonexisting = true,
+    weeklies_create_nonexisting = true,
+
+    -- template for new notes (new_note, follow_link)
+    -- set to `nil` or do not specify if you do not want a template
+    template_new_note = home .. '/' .. 'templates/basic.md',
+
+    -- template for newly created daily notes (goto_today)
+    -- set to `nil` or do not specify if you do not want a template
+    template_new_daily = home .. '/' .. 'templates/daily.md',
+
+    -- template for newly created weekly notes (goto_thisweek)
+    -- set to `nil` or do not specify if you do not want a template
+    template_new_weekly= home .. '/' .. 'templates/weekly.md',
+
+    -- image link style
+    -- wiki:     ![[image name]]
+    -- markdown: ![](image_subdir/xxxxx.png)
+    image_link_style = "markdown",
+
+    -- integrate with calendar-vim
+    plug_into_calendar = true,
+    calendar_opts = {
+        -- calendar week display mode: 1 .. 'WK01', 2 .. 'WK 1', 3 .. 'KW01', 4 .. 'KW 1', 5 .. '1'
+        weeknm = 4,
+        -- use monday as first day of week: 1 .. true, 0 .. false
+        calendar_monday = 1,
+        -- calendar mark: where to put mark for marked days: 'left', 'right', 'left-fit'
+        calendar_mark = 'left-fit',
+    },
+
+    -- telescope actions behavior
+    close_after_yanking = false,
+    insert_after_inserting = true,
+
+    -- tag notation: '#tag', ':tag:', 'yaml-bare'
+    tag_notation = "#tag",
+
+    -- command palette theme: dropdown (window) or ivy (bottom panel)
+    command_palette_theme = "ivy",
+
+    -- tag list theme:
+    -- get_cursor: small tag list at cursor; ivy and dropdown like above
+    show_tags_theme = "ivy",
+
+    -- when linking to a note in subdir/, create a [[subdir/title]] link
+    -- instead of a [[title only]] link
+    subdirs_in_links = true,
+
+    -- template_handling
+    -- What to do when creating a new note via `new_note()` or `follow_link()`
+    -- to a non-existing note
+    -- - prefer_new_note: use `new_note` template
+    -- - smart: if day or week is detected in title, use daily / weekly templates (default)
+    -- - always_ask: always ask before creating a note
+    template_handling = "smart",
+
+    -- path handling:
+    --   this applies to:
+    --     - new_note()
+    --     - new_templated_note()
+    --     - follow_link() to non-existing note
+    --
+    --   it does NOT apply to:
+    --     - goto_today()
+    --     - goto_thisweek()
+    --
+    --   Valid options:
+    --     - smart: put daily-looking notes in daily, weekly-looking ones in weekly,
+    --              all other ones in home, except for notes/with/subdirs/in/title.
+    --              (default)
+    --
+    --     - prefer_home: put all notes in home except for goto_today(), goto_thisweek()
+    --                    except for notes with subdirs/in/title.
+    --
+    --     - same_as_current: put all new notes in the dir of the current note if
+    --                        present or else in home
+    --                        except for notes/with/subdirs/in/title.
+    new_note_location = "smart",
+
+    -- should all links be updated when a file is renamed
+    rename_update_links = true,
 })
 
 return config
