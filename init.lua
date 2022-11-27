10f289feaedcc22a2e253c9d58827792c9b624b3
Albert <reg16vp@mail.ru>
2022-11-27 17:17:15 +0300

    add hop and neoscroll configs

--- init.lua
+++ init.lua
@@ -362,4 +362,33 @@ local config = {
     -- }
   end,
 }
+
+--HOP
+local status_ok, hop = pcall(require, "hop")
+if not status_ok then
+	return
+end
+hop.setup()
+
+local opts = { noremap = true, silent = true }
+local keymap = vim.api.nvim_set_keymap
+
+--neoscroll
+local status_ok, neoscroll = pcall(require, "neoscroll")
+if not status_ok then
+  return
+end
+
+neoscroll.setup {
+  -- All these keys will be mapped to their corresponding default scrolling animation
+  mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+  hide_cursor = true, -- Hide cursor while scrolling
+  stop_eof = true, -- Stop at <EOF> when scrolling downwards
+  use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+  respect_scrolloff = true, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+  cursor_scrolls_alone = false, -- The cursor will keep on scrolling even if the window cannot scroll further
+}
+
+
+
 return config
